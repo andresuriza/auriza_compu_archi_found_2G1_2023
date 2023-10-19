058f287bfccfa733fddd799a484c91cab4967951
Andres Uriza <urizandres@gmail.com>
2023-10-19 14:03:02 -0600

    Changes SPI master

--- SPI/SPI.ino
+++ SPI/SPI.ino
@@ -1,44 +1,43 @@
 #include <SPI.h>
 
-void setup(void)
+void setup (void)
 {
   Serial.begin (115200);
   Serial.println ();
   
-  digitalWrite(SS, HIGH);  
+  digitalWrite(SS, HIGH);  // ensure SS stays high for now
 
+  // Put SCK, MOSI, SS pins into output mode
+  // also put SCK, MOSI into LOW state, and SS into HIGH state.
+  // Then put SPI hardware into Master mode and turn SPI on
   SPI.begin ();
 
-  SPI.setClockDivider(SPI_CLOCK_DIV8); // Clock del master
-}
+  // Slow down the master a bit
+  SPI.setClockDivider(SPI_CLOCK_DIV8);
+  
+}  // end of setup
 
-byte transferAndWait(const byte what)
+byte transferAndWait (const byte what) // Transfiere numero del 0 al 15 en binario.
 {
-  byte a = SPI.transfer (what);
-  delayMicroseconds (20);
+  Serial.println(what, BIN);
+  byte a = SPI.transfer(what);
+  delayMicroseconds(20);
   return a;
-} 
+} // end of transferAndWait
 
-void loop(void)
+void loop (void)
 {
-  byte a, b, c, d;
+  byte a;
   
-  digitalWrite(SS, LOW); // Se activa el chip select   
+  // enable Slave Select
+  digitalWrite(SS, LOW);    
 
-  transferAndWait('a');
-  transferAndWait(10);
-  a = transferAndWait(17);
-  b = transferAndWait(33);
-  c = transferAndWait(42);
-  d = transferAndWait(0);
+  a = transferAndWait(2);
 
-  digitalWrite(SS, HIGH); // Se desactiva el chip select
+  // disable Slave Select
+  digitalWrite(SS, HIGH);
 
-  Serial.println ("Sumando resultados:");
-  Serial.println (a, DEC);
-  Serial.println (b, DEC);
-  Serial.println (c, DEC);
-  Serial.println (d, DEC);
+  Serial.println (a, DEC); // Imprime numero que retorna el slave
   
-  delay (1000); 
-}
+  delay (1000);  // 1 second delay 
+}  // end of loop
