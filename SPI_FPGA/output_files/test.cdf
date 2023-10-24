1ebca1fb8e81c00b2ae7721afccf948c5d27ab55
Andres u <urizandres@estudiantec.cr>
2023-10-24 00:26:50 -0600

    Files para Taller

--- SPI_FPGA/output_files/test.cdf
+++ SPI_FPGA/output_files/test.cdf
@@ -6,7 +6,7 @@ JedecChain;
 	P ActionCode(Ign)
 		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
 	P ActionCode(Cfg)
-		Device PartName(5CSEBA6U23) Path("C:/Users/Andres/Desktop/test/output_files/") File("test.sof") MfrSpec(OpMask(1));
+		Device PartName(5CSEBA6U23) Path("C:/Users/andres/Documents/TEC/Fundamentos de arquitectura/Proyecto 2/auriza_compu_archi_found_2G1_2023/SPI_FPGA/output_files/") File("test.sof") MfrSpec(OpMask(1));
 
 ChainEnd;
 
--- SPI_arduino/SPI_arduino.ino
+++ SPI_arduino/SPI_arduino.ino
@@ -1,31 +1,74 @@
+#include <SPI.h>
+
 int chipSel = 10;
 int clockPin = 13;
 int dataPin = 11;
 int miso = 12;
+int reset = 9;
+int ready = 8;
+int done = 7;
+int start = 6;
 
 void setup() {
   pinMode(chipSel, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(miso, INPUT);
+  pinMode(reset, OUTPUT);
+  pinMode(ready, OUTPUT);
+  pinMode(done, OUTPUT);
+  pinMode(start, INPUT);
+  SPI.begin();
   Serial.begin(9600);
-
-}
-
-void loop() {
-  digitalWrite(chipSel, LOW);
-
-  shiftOut(dataPin, clockPin, MSBFIRST, 4);
-
-  //byte incoming = shiftIn(miso, clockPin, MSBFIRST);
   
-
-  //Serial.println(incoming, BIN);
-
+  digitalWrite(chipSel, LOW);
+  handshake();
   digitalWrite(chipSel, HIGH);
-
-  delay(1000);
 }
+
+void handshake() {
+  digitalWrite(reset, HIGH);
+  digitalWrite(ready, LOW);
+  digitalWrite(done, LOW);
+  delay(100);
+
+  digitalWrite(reset, LOW);
+  digitalWrite(ready, HIGH);
+  digitalWrite(done, LOW); 
+  delay(100);
+
+  if (digitalRead(start) == 1) {
+    Serial.println("Handshake recibido");
+    byte b = SPI.transfer(4);  //TODO if == mensaje enviado, recibido
+    delayMicroseconds(200);
+    Serial.println("LEIDO: ");
+    Serial.println(b, BIN);
+    
+    digitalWrite(chipSel, HIGH);
+    delay(100); 
+  
+    digitalWrite(reset, HIGH);
+    delay(100);
+    digitalWrite(reset, LOW);
+    digitalWrite(ready, LOW);
+    digitalWrite(done, HIGH);
+    delay(100);
+
+    digitalWrite(reset, HIGH);
+    delay(100);
+    digitalWrite(reset, LOW);
+    digitalWrite(ready, LOW);
+    digitalWrite(done, LOW);
+    delay(100);
+  } else {
+    Serial.println("Fallido");
+    delay(3000);
+    handshake();
+  }
+}
+    
+  void loop() {
+  }
 
 
 /* Para incrementar el register
