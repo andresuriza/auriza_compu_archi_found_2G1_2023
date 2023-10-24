6394a510db0b87ee4388a5cd20cbf65c6ae7c69e
stphnVi <stephanie1117@hotmail.es>
2023-10-23 20:32:07 -0700

    testbench changes

--- SPI_FPGA/Verilog_files/handshake_tb.sv
+++ SPI_FPGA/Verilog_files/handshake_tb.sv
@@ -1,55 +1,66 @@
 module handshake_tb();
-	
-	logic clk, rst, rdy, dn;
-	reg st;
-	
-	handshake hsk(clk, rst, rdy, dn, st);
-	
-	initial begin 
-		clk = 0;
-		rst = 0;
-		rdy = 0;
-		dn = 0;
-		#40
-		clk = 1;
-		dn = 0;
-		#40
-	   clk = 0;
-		rst = 1;
-		rdy = 0;
-		dn = 0;
-		#40
-		clk = 1;
-		dn = 0;
-		#40
-		clk = 0;
-		rst = 0;
-		rdy = 0;
-		dn = 0;
-		#40
-		clk = 1;
-		dn = 0;
-		#40
-		clk = 0;
-		rdy = 1;
-		dn = 0;
-		#40
-		clk = 1;
-		dn = 0;
-		#40
-		clk = 0;
-		rdy = 0;
-		dn = 1;
-		#40
-		clk = 1;
-		dn = 1;
-		#40
-		clk = 0;
-		dn = 0;
-		#40
-		clk = 1;
-		dn = 0;
-	end
-	
+   reg clk;         
+   reg rst_l;       
+   reg ready;       
+   reg done;        // Señal de done
+   wire start;      // Señal de start generada por el módulo handshake
 
+
+   handshake hsk(
+      .clk(clk),
+      .rst_l(rst_l),
+      .ready(ready),
+      .done(done),
+      .start(start)
+   );
+
+   initial begin
+
+      clk = 0;
+      rst_l = 1;
+      ready = 0;
+      done = 0;
+		
+		
+      #10 rst_l = 0;
+      ready = 1;
+      done = 0;
+      #10
+      if (start == 1)
+         $display("Handshake iniciado correctamente.");
+      else
+         $display("Handshake no iniciado.");
+
+		#10 rst_l = 1;
+		#10 rst_l = 0;
+		ready = 0;
+      done = 1;
+		#10
+		if (start == 1)
+         $display("Handshake iniciado correctamente.");
+      else
+         $display("Handshake no iniciado.");
+			
+		#10 rst_l = 1;
+		#10 rst_l = 0;
+		ready = 1;
+      done = 1;
+		if (start == 1)
+         $display("Handshake iniciado correctamente.");
+      else
+         $display("Handshake no iniciado.");
+			
+		#10 rst_l = 1;
+		#10 rst_l = 0;
+		ready = 0;
+      done = 0;
+		if (start == 1)
+         $display("Handshake iniciado correctamente.");
+      else
+         $display("Handshake no iniciado.");
+		
+      $finish;
+   end
+
+   always #5 clk = ~clk;  // Generación del reloj
 endmodule
