240ec1b9ad0755e865b3b3691cfce47d4a9c8872
stphnVi <stephanie1117@hotmail.es>
2023-10-25 18:11:47 -0700

    Revert "Merge remote-tracking branch 'origin/develop' into develop"
    This reverts commit c15a84636a675f6031a45499f807704a84010419, reversing
    changes made to 8305183a158b13df3035fcd7939dfb0c3fcfcf92.

--- SPI_FPGA/Verilog_files/recievesend.sv
+++ SPI_FPGA/Verilog_files/recievesend.sv
@@ -1,12 +1,11 @@
-module recievesend(input MOSI , clk, rst, ready, done, cs,
-						 output MISO, start,
+module recievesend(input MOSI , clk, cs,
+						 output MISO, 
 						 output reg [3:0]lds);
 						 
 			logic mosi;
 			
 			assign mosi = ~cs&MOSI;
-			
-			handshake hk(clk, rst, ready, done, start);
+						 
 			SPregister regist(mosi, clk, lds);
 			flipflop ff(mosi, clk, MISO);
 	
