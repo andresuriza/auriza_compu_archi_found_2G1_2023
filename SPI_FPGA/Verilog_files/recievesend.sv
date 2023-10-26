c5f1a3aa779871e053f23af622165f472fe0bb18
Dennis Jimenez <alejimenezc@hotmail.com>
2023-10-26 15:25:34 -0600

    recievesend fix

--- SPI_FPGA/Verilog_files/recievesend.sv
+++ SPI_FPGA/Verilog_files/recievesend.sv
@@ -1,11 +1,13 @@
-module recievesend(input MOSI , clk, cs,
+module recievesend(input MOSI , clk, cs, rst, rdy, dn, 
 						 output MISO, 
-						 output reg [3:0]lds);
+						 output reg [3:0]lds,
+						 output reg start);
 						 
 			logic mosi;
 			
 			assign mosi = ~cs&MOSI;
-						 
+			
+			handshake hs(clk, rst, rdy, dn, start);
 			SPregister regist(mosi, clk, lds);
 			flipflop ff(mosi, clk, MISO);
 	
