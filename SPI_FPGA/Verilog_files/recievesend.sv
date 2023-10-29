762612a19a339bf9ea562a2f55248519d1bcd212
Dennis Jimenez <alejimenezc@hotmail.com>
2023-10-29 14:10:09 -0600

    PWM integration

--- SPI_FPGA/Verilog_files/recievesend.sv
+++ SPI_FPGA/Verilog_files/recievesend.sv
@@ -1,12 +1,14 @@
 module recievesend(input MOSI , clk, cs, rst, rdy, dn, 
 						 output MISO, 
 						 output reg [3:0]lds,
-						 output reg start);
+						 output reg start,
+						 output reg speed);
 						 
 			logic mosi;
 			
 			assign mosi = ~cs&MOSI;
 			
+			pwm p(clk,lds,speed);
 			handshake hs(clk, rst, rdy, dn, start);
 			SPregister regist(mosi, clk, lds);
 			flipflop ff(mosi, clk, MISO);
