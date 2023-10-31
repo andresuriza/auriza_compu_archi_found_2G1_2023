9fdb21f7ba3f34b0eb35a2476d0ef17bcfa68034
Dennis Jimenez <alejimenezc@hotmail.com>
2023-10-30 22:44:40 -0600

    PWM fix

--- SPI_FPGA/Verilog_files/recievesend_tb.sv
+++ SPI_FPGA/Verilog_files/recievesend_tb.sv
@@ -1,37 +1,26 @@
 module recievesend_tb();
 
-	logic mosi,clk,cs;
+	logic mosi,clk,cs,rst,rdy,dn;
 	reg [3:0] lds;
+	reg speed, start;
 	wire miso;
 	
-	recievesend misomosi(mosi,clk,cs,miso,lds);
+	recievesend misomosi(mosi,clk,cs,rst,rdy,dn,miso,lds,start,speed);
+	
+	always begin
+        #1 clk = ~clk;
+    end
 	
 	initial begin
 	
-	mosi = 1;
 	clk = 0;
-	cs = 0;
-	#40
-	mosi = 1;
-	clk = 1;
-	#40
-	mosi = 0;
-	clk = 0;
-	#40
-	mosi = 0;
-	clk = 1;
-	#40
-	mosi = 1;
-	clk = 0;
-	#40
-	mosi = 1;
-	clk = 1;
-	#40
-	mosi = 1;
-	clk = 0;
-	#40
-	mosi = 1;
-	clk = 1;
+	
+	for (int i = 0; i <= 3; i = i + 1) begin
+            mosi = 1;
+            #150; // Espera un tiempo antes de cambiar el número binario
+        end
+
+        #50 $finish;
 	end
 
 endmodule
