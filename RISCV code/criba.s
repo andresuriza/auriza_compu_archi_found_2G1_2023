bf96d51f71477f3d0eb762ed5c5fa37df6b3a94f
Andres u <urizandres@estudiantec.cr>
2023-11-20 22:32:04 -0600

    comentarios+

--- RISCV code/criba.s
+++ RISCV code/criba.s
@@ -6,13 +6,13 @@
 main:
     li x1, 2 # i = 2
     li x2, 1 # resta
-    li x3, 0 # valor inicial
+    li x3, 0 # valor inicial 
     li x8, 2 # Numero 2
     lw x7, n # limite
     addi x7, x7(1)
     sub x4, x3, x2 # n-1
  
- Criba:
+Criba:
      beq x3, x7, Exit # Si v inicial == v final, termine
      j EsPrimo
           
@@ -20,6 +20,7 @@ EsPrimo:
     beq x3, x0, NoEsPrimo # Ignorar 0
     beq x3, x2, NoEsPrimo # Ignorar 1
     beq x3, x8, SiEsPrimo # 2 siempre es primo
+    
     beq x1, x4, SiEsPrimo # Si i es igual a n-1, parar
     rem x5, x3, x1 # n % i
     beq x5, x0, NoEsPrimo # Si n % i es 0, parar
@@ -27,9 +28,8 @@ EsPrimo:
     j EsPrimo
 
 NoEsPrimo:
-    li x1, 2
-    addi x3, x3, 1
-    li x1, 2 # i = 2 
+    li x1, 2 # i = 2
+    addi x3, x3, 1 # valor inicial +1
     sub x4, x3, x2 # n-1
     j Criba
 
